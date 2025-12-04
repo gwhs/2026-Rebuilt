a2344260971d37ad87b0ca1af4319521ab4b332f
Emma "R1shy" Kadlecek <kadlecekemma@gmail.com>
2025-12-03 21:56:23 -0800

    .

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2026-rebuilt";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 9;
+  public static final String GIT_SHA = "704aa210b75312ec603d959b05c3da7b4dbe6163";
+  public static final String GIT_DATE = "04-12-25 05:04:53";
+  public static final String GIT_BRANCH = "cp-workflows";
+  public static final String BUILD_DATE = "04-12-25 05:40:28";
+  public static final long BUILD_UNIX_TIME = 1764826828184L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
