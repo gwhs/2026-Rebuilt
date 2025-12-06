af798f03616b27bd3c878443c1fdc35a6d84cf2b
Emma Kadlecek <kadlecekemma@gmail.com>
2025-12-06 00:00:25 -0800

    .

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -47,7 +47,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
 
-      Threads.setCurrentThreadPriority(true, 99);
+    Threads.setCurrentThreadPriority(true, 99);
 
     double startTime = HALUtil.getFPGATime();
 
