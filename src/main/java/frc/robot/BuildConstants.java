384eef75e4449ae171c9bcb2a09eba019eb5110a
Emma Kadlecek <kadlecekemma@gmail.com>
2025-12-08 09:05:05 -0800

    issac changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 10;
-  public static final String GIT_SHA = "c0d3bf2ce6c855acb42bc2e7f242c6c287b42940";
-  public static final String GIT_DATE = "08-12-25 06:05:54";
+  public static final int GIT_REVISION = 11;
+  public static final String GIT_SHA = "426c8aa457551ed77651b5784e0505bc3c88ed17";
+  public static final String GIT_DATE = "08-12-25 06:11:47";
   public static final String GIT_BRANCH = "auton-stuff";
-  public static final String BUILD_DATE = "08-12-25 06:09:58";
-  public static final long BUILD_UNIX_TIME = 1765174198927L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "08-12-25 17:03:14";
+  public static final long BUILD_UNIX_TIME = 1765213394264L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
