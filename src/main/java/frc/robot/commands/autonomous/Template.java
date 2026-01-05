e2f4a6dd219f728d90bc20e965eb243954d5b948
Emma "R1shy" Kadlecek <kadlecekemma@gmail.com>
2026-01-05 14:20:43 -0800

    .

--- src/main/java/frc/robot/commands/autonomous/Template.java
+++ src/main/java/frc/robot/commands/autonomous/Template.java
@@ -1,26 +1,35 @@
-package frc.robot.commands.autonomous;
+package frc.robot.autonomous;
 
 import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.RobotContainer;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-public class Template extends PathPlannerAuto {
-  public Template(RobotContainer robotContainer) {
-    super(Commands.run(() -> {}));
+public class Template extends SequentialCommandGroup {
+  public Template() {
 
     /* All your code should go inside this try-catch block */
     try {
 
-      PathPlannerPath S3Leave = PathPlannerPath.fromPathFile("S3-C5");
+      /*
+        TODO: Load Paths
+      */
+      PathPlannerPath startingPath = PathPlannerPath.fromChoreoTrajectory("PATH NAME");
+      // PathPlannerPath another_path = PathPlannerPath.fromChoreoTrajectory("PATH NAME");
 
       Pose2d startingPose =
-          new Pose2d(S3Leave.getPoint(0).position, S3Leave.getIdealStartingState().rotation());
+          new Pose2d(
+              startingPath.getPoint(0).position, startingPath.getIdealStartingState().rotation());
 
-      isRunning().onTrue(Commands.sequence(AutoBuilder.resetOdom(startingPose)));
+      addCommands(
+          AutoBuilder.resetOdom(startingPose).onlyIf(() -> RobotBase.isSimulation()),
+          AutoBuilder.followPath(startingPath)
+          /*
+           * TODO: The rest of the autonomous routine command
+           */
+          );
 
     } catch (Exception e) {
       DriverStation.reportError("Path Not Found: " + e.getMessage(), e.getStackTrace());
