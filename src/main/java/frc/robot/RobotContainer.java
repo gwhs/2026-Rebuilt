47cd662788a3318683913259739fa6298d2398fe
Alvin G. <alguo1@s.sfusd.edu>
2026-01-07 16:28:18 -0800

    commit

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,9 @@ import edu.wpi.first.hal.HALUtil;
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -62,6 +64,8 @@ public class RobotContainer {
 
     PathfindingCommand.warmupCommand().schedule();
 
+    SmartDashboard.putData("Command Scheduler", CommandScheduler.getInstance());
+    
     addPeriodic.accept(() -> {}, 0.5);
   }
 
