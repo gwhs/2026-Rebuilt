9b3c35215572d149c50bc8a243831879190669c0
bella <betan1@s.sfusd.edu>
2026-01-14 17:14:54 -0800

    updated getRobotTargetAngle methood logic

--- src/main/java/frc/robot/EagleUtil.java
+++ src/main/java/frc/robot/EagleUtil.java
@@ -2,25 +2,16 @@ package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
 
 public class EagleUtil {
-    public static double radianToDegree(double radian)
-    {
-        return radian * (180 / Math.PI);
-    }
-    public static double degreeToRadian(double degree)
-    {
-        return degree * (Math.PI / 180);
-    }
     public static double getRobotTargetAngle(Pose2d robotpose, Translation2d target)
     {
-        double dx = robotpose.getX() - target.getX();
-        double dy = robotpose.getY() - target.getY();
-        double theta = radianToDegree(Math.atan(dy/dx));
-        if (dx < 0)
-        {
-            theta = 180 - theta;
-        }
-        return theta;
+        return target.minus(robotpose.getTranslation()).getAngle().getDegrees();
+    }
+
+    public static double getFlywheelSpeed (double distance)
+    {
+        return 0; //update
     }
 }
