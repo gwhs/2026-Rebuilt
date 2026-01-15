460a1b91855990658f46da5732b5dcf0c74e10d4
Caden Chen <Cachen15@s.sfusd.edu>
2026-01-14 17:28:57 -0800

    Efficiency Adjustments (working model)

--- src/main/java/frc/robot/commands/autonomous/BumpPathAuto.java
+++ src/main/java/frc/robot/commands/autonomous/BumpPathAuto.java
@@ -17,46 +17,31 @@ public class BumpPathAuto extends SequentialCommandGroup {
       /*
         TODO: Load Paths
       */
-      PathPlannerPath startingPath = PathPlannerPath.fromChoreoTrajectory("Unload");
-      PathPlannerPath cross1Path = PathPlannerPath.fromChoreoTrajectory("Cross1");
-      PathPlannerPath gather1Path = PathPlannerPath.fromChoreoTrajectory("Gather1");
-      PathPlannerPath return1Path = PathPlannerPath.fromChoreoTrajectory("Return1");
-      PathPlannerPath score1Path = PathPlannerPath.fromChoreoTrajectory("Score1");
-      PathPlannerPath cross2Path = PathPlannerPath.fromChoreoTrajectory("Cross2");
-      PathPlannerPath gather2Path = PathPlannerPath.fromChoreoTrajectory("Gather2");
-      PathPlannerPath return2Path = PathPlannerPath.fromChoreoTrajectory("Return2");
-      PathPlannerPath score2Path = PathPlannerPath.fromChoreoTrajectory("Score2");
+      PathPlannerPath cyclePath = PathPlannerPath.fromChoreoTrajectory("Cycle");
+      PathPlannerPath climbPath = PathPlannerPath.fromChoreoTrajectory("Climb");
 
       // PathPlannerPath another_path = PathPlannerPath.fromChoreoTrajectory("PATH NAME");
 
       Pose2d startingPose =
-          new Pose2d(
-              startingPath.getPoint(0).position, startingPath.getIdealStartingState().rotation());
+          new Pose2d(cyclePath.getPoint(0).position, cyclePath.getIdealStartingState().rotation());
 
       addCommands(
           AutoBuilder.resetOdom(startingPose).onlyIf(() -> RobotBase.isSimulation()),
-          Commands.waitSeconds(3), // score preloaded fuel
-          Commands.parallel(
-              AutoBuilder.followPath(cross1Path),
-              Commands.sequence(
-                  Commands.waitSeconds(0.8)
-                  // deploy ground intake
-                  )),
-          AutoBuilder.followPath(gather1Path),
+          AutoBuilder.followPath(cyclePath),
           // stow/protect ground intake
-          AutoBuilder.followPath(return1Path),
+
           Commands.waitSeconds(3.0), // score
-          AutoBuilder.followPath(score1Path),
+          AutoBuilder.followPath(cyclePath),
+          /*
           Commands.parallel(
               AutoBuilder.followPath(cross2Path),
               Commands.sequence(
                   Commands.waitSeconds(0.8)
                   // deploy ground intake
                   )),
-          AutoBuilder.followPath(gather2Path),
+          */
+          AutoBuilder.followPath(climbPath),
           // stow/protect ground intake
-          AutoBuilder.followPath(return2Path),
-          AutoBuilder.followPath(score2Path),
           Commands.waitSeconds(3.0)
           /*
            * TODO: The rest of the autonomous routine command
