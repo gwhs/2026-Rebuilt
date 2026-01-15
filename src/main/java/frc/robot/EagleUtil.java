900710530380e94b75df8b03b7f6443c30051ef4
Emma Kadlecek <kadlecekemma@gmail.com>
2026-01-14 18:47:27 -0800

    .

--- src/main/java/frc/robot/EagleUtil.java
+++ src/main/java/frc/robot/EagleUtil.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
+<<<<<<< HEAD
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.robot.subsystems.swerve.SwerveSubsystem.RotationTarget;
@@ -32,5 +33,15 @@ public class EagleUtil {
     }
     alignmentTarget = FieldConstants.BLUE_OUTPOST;
     return alignmentTarget.minus(pose.getTranslation()).getAngle().getDegrees();
+=======
+
+public class EagleUtil {
+  public static double getRobotTargetAngle(Pose2d robotpose, Translation2d target) {
+    return target.minus(robotpose.getTranslation()).getAngle().getDegrees();
+  }
+
+  public static double getRobotTargetDistance(Pose2d robotpose, Translation2d target) {
+    return target.getDistance(robotpose.getTranslation());
+>>>>>>> f80bdd4 (added getRobotTargetAngle and getRobotTargetDistance methood (#20))
   }
 }
