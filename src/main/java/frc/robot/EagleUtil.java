7b2a74b0ad17d5a964400ca3baf3c20ad945e26c
Emma Kadlecek <kadlecekemma@gmail.com>
2026-01-14 17:11:47 -0800

    .

--- src/main/java/frc/robot/EagleUtil.java
+++ src/main/java/frc/robot/EagleUtil.java
@@ -2,25 +2,11 @@ package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.robot.subsystems.swerve.SwerveSubsystem.RotationTarget;
 
 public class EagleUtil {
-
-  private static double RED_DEPOTX = Units.inchesToMeters(2.014);
-  private static double RED_DEPOTY = Units.inchesToMeters(6.704);
-  private static double BLUE_DEPOTX = Units.inchesToMeters(9.604);
-  private static double BLUE_DEPOTY = Units.inchesToMeters(7.602);
-  private static double RED_OUTPOSTX = Units.inchesToMeters(1.845);
-  private static double RED_OUTPOSTY = Units.inchesToMeters(1.976);
-  private static double BLUE_OUTPOSTX = Units.inchesToMeters(9.535);
-  private static double BLUE_OUTPOSTY = Units.inchesToMeters(0.888);
-  private static Translation2d RED_DEPOT = new Translation2d(RED_DEPOTX, RED_DEPOTY);
-  private static Translation2d BLUE_DEPOT = new Translation2d(BLUE_DEPOTX, BLUE_DEPOTY);
-  private static Translation2d RED_OUTPOST = new Translation2d(RED_OUTPOSTX, RED_OUTPOSTY);
-  private static Translation2d BLUE_OUTPOST = new Translation2d(BLUE_OUTPOSTX, BLUE_OUTPOSTY);
 
   public static boolean isRedAlliance() {
     return DriverStation.getAlliance().isPresent()
@@ -31,20 +17,20 @@ public class EagleUtil {
     Translation2d alignmentTarget;
     if (target == RotationTarget.PASSING_DEPOT_SIDE) {
       if (isRedAlliance()) {
-        alignmentTarget = RED_DEPOT;
+        alignmentTarget = FieldConstants.RED_DEPOT;
 
         return alignmentTarget.minus(pose.getTranslation()).getAngle().getDegrees();
       }
-      alignmentTarget = BLUE_DEPOT;
+      alignmentTarget = FieldConstants.BLUE_DEPOT;
 
       return alignmentTarget.minus(pose.getTranslation()).getAngle().getDegrees();
     }
     if (isRedAlliance()) {
-      alignmentTarget = RED_OUTPOST;
+      alignmentTarget = FieldConstants.RED_OUTPOST;
 
       return alignmentTarget.minus(pose.getTranslation()).getAngle().getDegrees();
     }
-    alignmentTarget = BLUE_OUTPOST;
+    alignmentTarget = FieldConstants.BLUE_OUTPOST;
     return alignmentTarget.minus(pose.getTranslation()).getAngle().getDegrees();
   }
 }
--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -1,3 +1,20 @@
 package frc.robot;
 
-public class FieldConstants {}
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+
+public class FieldConstants {
+
+  public static double RED_DEPOTX = Units.inchesToMeters(2.014);
+  public static double RED_DEPOTY = Units.inchesToMeters(6.704);
+  public static double BLUE_DEPOTX = Units.inchesToMeters(9.604);
+  public static double BLUE_DEPOTY = Units.inchesToMeters(7.602);
+  public static double RED_OUTPOSTX = Units.inchesToMeters(1.845);
+  public static double RED_OUTPOSTY = Units.inchesToMeters(1.976);
+  public static double BLUE_OUTPOSTX = Units.inchesToMeters(9.535);
+  public static double BLUE_OUTPOSTY = Units.inchesToMeters(0.888);
+  public static Translation2d RED_DEPOT = new Translation2d(RED_DEPOTX, RED_DEPOTY);
+  public static Translation2d BLUE_DEPOT = new Translation2d(BLUE_DEPOTX, BLUE_DEPOTY);
+  public static Translation2d RED_OUTPOST = new Translation2d(RED_OUTPOSTX, RED_OUTPOSTY);
+  public static Translation2d BLUE_OUTPOST = new Translation2d(BLUE_OUTPOSTX, BLUE_OUTPOSTY);
+}
