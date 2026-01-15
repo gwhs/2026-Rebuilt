121b1554bbb5124d47560c0f970d78822dfbec02
bella <betan1@s.sfusd.edu>
2026-01-14 18:11:03 -0800

    added getRobotTargetAnglr and getRobotTargetDistance methood

--- src/main/java/frc/robot/EagleUtil.java
+++ src/main/java/frc/robot/EagleUtil.java
@@ -1,3 +1,14 @@
 package frc.robot;
 
-public class EagleUtil {}
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+
+public class EagleUtil {
+  public static double getRobotTargetAngle(Pose2d robotpose, Translation2d target) {
+    return target.minus(robotpose.getTranslation()).getAngle().getDegrees();
+  }
+
+  public static double getRobotTargetDistance(Pose2d robotpose, Translation2d target) {
+    return target.getDistance(robotpose.getTranslation());
+  }
+}
