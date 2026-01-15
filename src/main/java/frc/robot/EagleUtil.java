bcc0c9923890ad01cfc696a419460862442e7bc9
Emma Kadlecek <kadlecekemma@gmail.com>
2026-01-14 17:04:20 -0800

    .

--- src/main/java/frc/robot/EagleUtil.java
+++ src/main/java/frc/robot/EagleUtil.java
@@ -1,3 +1,53 @@
 package frc.robot;
 
-public class EagleUtil {}
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import frc.robot.subsystems.swerve.SwerveSubsystem.RotationTarget;
+
+public class EagleUtil {
+
+
+
+	private static double RED_DEPOTX = Units.inchesToMeters(2.014);
+	private static double RED_DEPOTY = Units.inchesToMeters(6.704);
+	private static double BLUE_DEPOTX = Units.inchesToMeters(9.604);
+	private static double BLUE_DEPOTY = Units.inchesToMeters(7.602);
+	private static double RED_OUTPOSTX = Units.inchesToMeters(1.845);
+	private static double RED_OUTPOSTY = Units.inchesToMeters(1.976);
+	private static double BLUE_OUTPOSTX = Units.inchesToMeters(9.535);
+	private static double BLUE_OUTPOSTY = Units.inchesToMeters(0.888);
+	private static Translation2d RED_DEPOT = new Translation2d(RED_DEPOTX, RED_DEPOTY);	
+	private static Translation2d BLUE_DEPOT = new Translation2d(BLUE_DEPOTX, BLUE_DEPOTY);	
+	private static Translation2d RED_OUTPOST = new Translation2d(RED_OUTPOSTX, RED_OUTPOSTY);	
+	private static Translation2d BLUE_OUTPOST = new Translation2d(BLUE_OUTPOSTX, BLUE_OUTPOSTY);	
+
+
+ public static boolean isRedAlliance() {
+    return DriverStation.getAlliance().isPresent()
+        && DriverStation.getAlliance().get() == Alliance.Red;
+  }
+
+	public static double getTarget(RotationTarget target, Pose2d pose) {
+		Translation2d alignmentTarget;
+		if (target == RotationTarget.PASSING_DEPOT_SIDE) {
+			if(isRedAlliance()) {
+				alignmentTarget = RED_DEPOT;
+
+				return alignmentTarget.minus(pose.getTranslation()).getAngle().getDegrees();
+			}
+			alignmentTarget = BLUE_DEPOT;
+
+				return alignmentTarget.minus(pose.getTranslation()).getAngle().getDegrees();
+		}
+			if (isRedAlliance()) {
+				alignmentTarget = RED_OUTPOST;
+
+		return alignmentTarget.minus(pose.getTranslation()).getAngle().getDegrees();
+			}
+			alignmentTarget = BLUE_OUTPOST;	
+		return alignmentTarget.minus(pose.getTranslation()).getAngle().getDegrees();
+	}
+}
--- src/main/java/frc/robot/subsystems/swerve/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveSubsystem.java
@@ -26,6 +26,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.EagleUtil;
 import frc.robot.commands.AlignToPose;
 import java.util.function.Supplier;
 
@@ -38,7 +39,9 @@ public class SwerveSubsystem extends SwerveDrivetrain<TalonFX, TalonFX, CANcoder
 
   public enum RotationTarget {
     NORMAL,
-    FORTY_FIVE
+    FORTY_FIVE,
+    PASSING_DEPOT_SIDE,
+    PASSING_OUTPOST_SIDE
   }
 
   private RotationTarget rotationTarget = RotationTarget.NORMAL;
@@ -205,6 +208,10 @@ public class SwerveSubsystem extends SwerveDrivetrain<TalonFX, TalonFX, CANcoder
         return 45.0;
       case NORMAL:
         return 0;
+      case PASSING_DEPOT_SIDE:
+	return EagleUtil.getTarget(RotationTarget.PASSING_DEPOT_SIDE, getState().Pose);
+      case PASSING_OUTPOST_SIDE:
+	return EagleUtil.getTarget(RotationTarget.PASSING_OUTPOST_SIDE, getState().Pose);
       default:
         return 0;
     }
