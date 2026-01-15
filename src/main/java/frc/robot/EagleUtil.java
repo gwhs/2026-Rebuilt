fa852000e7f4ae6cbc4a0673b0a0ad51b980735e
bella <betan1@s.sfusd.edu>
2026-01-14 16:06:43 -0800

    added getRobotTargetAngle methood

--- src/main/java/frc/robot/EagleUtil.java
+++ src/main/java/frc/robot/EagleUtil.java
@@ -1,3 +1,30 @@
 package frc.robot;
 
-public class EagleUtil {}
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+
+public class EagleUtil {
+    public static double radianToDegree(double radian)
+    {
+        return radian * (180 / Math.PI);
+    }
+    public static double degreeToRadian(double degree)
+    {
+        return degree * (Math.PI / 180);
+    }
+    public static double getRobotTargetAngle(Pose2d robotpose, Translation2d target)
+    {
+        double dx = target.getX() - robotpose.getX();
+        double dy = target.getY() - robotpose.getY();
+        double theta = radianToDegree(Math.atan(dy/dx));
+        if (dx < 0)
+        {
+            theta = 180 - theta;
+        }
+        if (dy < 0)
+        {
+            theta += 180;
+        }
+        return theta;
+    }
+}
