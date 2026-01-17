aad84b491befe980146eddbb5e47ac1f9705e68f
Caden740 <Cachen448@gmail.com>
2026-01-17 14:34:03 -0800

    .

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,132 +1,83 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
-import dev.doglog.DogLog;
-import dev.doglog.DogLogOptions;
-import edu.wpi.first.hal.HALUtil;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.PowerDistribution;
-import edu.wpi.first.wpilibj.Threads;
+import com.ctre.phoenix6.HootAutoReplay;
+
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import java.lang.management.GarbageCollectorMXBean;
-import java.lang.management.ManagementFactory;
-import java.util.List;
 
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+    private Command m_autonomousCommand;
 
-  private final RobotContainer m_robotContainer;
-  private double prevTime = HALUtil.getFPGATime();
-  private final GcStatsCollector gcStatsCollector = new GcStatsCollector();
+    private final RobotContainer m_robotContainer;
 
-  public Robot() {
-    // Setup DogLog
-    DogLog.setOptions(
-        new DogLogOptions().withNtPublish(true).withCaptureNt(true).withCaptureDs(true));
-    DogLog.setPdh(new PowerDistribution());
+    /* log and replay timestamp and joystick data */
+    private final HootAutoReplay m_timeAndJoystickReplay = new HootAutoReplay()
+        .withTimestampReplay()
+        .withJoystickReplay();
 
-    m_robotContainer = new RobotContainer(this::addPeriodic);
-
-    LiveWindow.disableAllTelemetry();
-
-    DriverStation.silenceJoystickConnectionWarning(true);
-
-    NetworkTableInstance.getDefault()
-        .getStringTopic("/Metadata/Branch")
-        .publish()
-        .set(BuildConstants.GIT_BRANCH);
-    NetworkTableInstance.getDefault()
-        .getStringTopic("/Metadata/SHA")
-        .publish()
-        .set(BuildConstants.GIT_SHA);
-  }
-
-  @Override
-  public void robotPeriodic() {
-    try {
-      Threads.setCurrentThreadPriority(true, 99);
-
-      double startTime = HALUtil.getFPGATime();
-      CommandScheduler.getInstance().run();
-      DogLog.log("Loop Time/Command Scheduler", (HALUtil.getFPGATime() - startTime) / 1000);
-      double endTime = HALUtil.getFPGATime();
-      m_robotContainer.periodic();
-      DogLog.log("Loop Time/Robot Container", (HALUtil.getFPGATime() - endTime) / 1000);
-
-      gcStatsCollector.update();
-
-      double currentTime = HALUtil.getFPGATime();
-      DogLog.log("Loop Time/Total", (currentTime - prevTime) / 1000);
-      prevTime = currentTime;
-    } finally {
-      Threads.setCurrentThreadPriority(false, 10);
+    public Robot() {
+        m_robotContainer = new RobotContainer();
     }
-  }
 
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-    if (m_autonomousCommand != null) {
-      CommandScheduler.getInstance().schedule(m_autonomousCommand);
+    @Override
+    public void robotPeriodic() {
+        m_timeAndJoystickReplay.update();
+        CommandScheduler.getInstance().run(); 
     }
-  }
 
-  @Override
-  public void autonomousPeriodic() {}
+    @Override
+    public void disabledInit() {}
 
-  @Override
-  public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
+    @Override
+    public void disabledPeriodic() {}
+
+    @Override
+    public void disabledExit() {}
+
+    @Override
+    public void autonomousInit() {
+        m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+        if (m_autonomousCommand != null) {
+            CommandScheduler.getInstance().schedule(m_autonomousCommand);
+        }
     }
-  }
 
-  @Override
-  public void teleopPeriodic() {}
+    @Override
+    public void autonomousPeriodic() {}
 
-  @Override
-  public void testInit() {
-    CommandScheduler.getInstance().cancelAll();
-  }
+    @Override
+    public void autonomousExit() {}
 
-  @Override
-  public void testPeriodic() {}
-
-  @Override
-  public void simulationInit() {}
-
-  @Override
-  public void simulationPeriodic() {}
-
-  private static final class GcStatsCollector {
-    private List<GarbageCollectorMXBean> gcBeans = ManagementFactory.getGarbageCollectorMXBeans();
-    private final long[] lastTimes = new long[gcBeans.size()];
-    private final long[] lastCounts = new long[gcBeans.size()];
-
-    public void update() {
-      long accumTime = 0;
-      long accumCounts = 0;
-      for (int i = 0; i < gcBeans.size(); i++) {
-        long gcTime = gcBeans.get(i).getCollectionTime();
-        long gcCount = gcBeans.get(i).getCollectionCount();
-        accumTime += gcTime - lastTimes[i];
-        accumCounts += gcCount - lastCounts[i];
-
-        lastTimes[i] = gcTime;
-        lastCounts[i] = gcCount;
-      }
-
-      DogLog.log("GC/GCTimeMS", (double) accumTime);
-      DogLog.log("GC/GCCounts", (double) accumCounts);
+    @Override
+    public void teleopInit() {
+        if (m_autonomousCommand != null) {
+            CommandScheduler.getInstance().cancel(m_autonomousCommand);
+        }
     }
-  }
+
+    @Override
+    public void teleopPeriodic() {}
+
+    @Override
+    public void teleopExit() {}
+
+    @Override
+    public void testInit() {
+        CommandScheduler.getInstance().cancelAll();
+    }
+
+    @Override
+    public void testPeriodic() {}
+
+    @Override
+    public void testExit() {}
+
+    @Override
+    public void simulationPeriodic() {}
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,148 +1,98 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
-import com.ctre.phoenix6.CANBus;
-import com.ctre.phoenix6.CANBus.CANBusStatus;
-import com.ctre.phoenix6.StatusSignalCollection;
-import com.pathplanner.lib.commands.PathfindingCommand;
-import dev.doglog.DogLog;
-import edu.wpi.first.hal.HALUtil;
-import edu.wpi.first.wpilibj.Alert;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import static edu.wpi.first.units.Units.*;
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.commands.DriveCommand;
-import frc.robot.subsystems.swerve.SwerveSubsystem;
-import frc.robot.subsystems.swerve.TunerConstants_Anemone;
-import frc.robot.subsystems.swerve.TunerConstants_mk4n;
-import java.util.function.BiConsumer;
+import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+
+import frc.robot.generated.TunerConstants;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class RobotContainer {
-  public enum Robot {
-    DEV,
-    COMP,
-    ANEMONE,
-    KITBOT
-  }
+    private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
-  private final SwerveSubsystem drivetrain;
-  public static final CommandXboxController controller = new CommandXboxController(0);
+    /* Setting up bindings for necessary control of the swerve drive platform */
+    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+    private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+    private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
 
-  private final DriveCommand defualtDriveCommand;
+    private final Telemetry logger = new Telemetry(MaxSpeed);
 
-  // TODO: update serial numbers
-  @SuppressWarnings("resource")
-  public static Robot getRobot() {
-    if (RobotController.getSerialNumber().equals("032414F0")) {
-      return Robot.ANEMONE;
-    } else if (RobotController.getSerialNumber().equals("03223849")) {
-      return Robot.DEV;
-    } else if (RobotController.getSerialNumber().equals("1234")) {
-      return Robot.COMP;
-    } else if (RobotController.getSerialNumber().equals("123")) {
-      return Robot.KITBOT;
-    } else {
-      new Alert(
-              "roborio unrecognized. here is the serial number:"
-                  + RobotController.getSerialNumber(),
-              Alert.AlertType.kError)
-          .set(true);
-      ;
-      return Robot.COMP;
-    }
-  }
+    private final CommandXboxController joystick = new CommandXboxController(0);
 
-  @SuppressWarnings("unused")
-  private final BiConsumer<Runnable, Double> addPeriodic;
+    public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
-  private final CANBus rioCanbus = new CANBus("rio");
-  private final CANBus canivoreCanbus = new CANBus("CANivore");
-
-  private final StatusSignalCollection signalList = new StatusSignalCollection();
-  //
-
-  private final RobotVisualizer robovisual = new RobotVisualizer();
-  private final SendableChooser<Command> autoChooser = new SendableChooser<Command>();
-
-  public RobotContainer(BiConsumer<Runnable, Double> addPeriodic) {
-
-    this.addPeriodic = addPeriodic;
-
-    addPeriodic.accept(
-        () -> {
-          CANBusStatus status = canivoreCanbus.getStatus();
-          DogLog.log("Canivore/Canivore Bus Utilization", status.BusUtilization);
-          DogLog.log("Canivore/Status Code on Canivore", status.Status.toString());
-        },
-        0.5);
-
-    switch (getRobot()) {
-      case COMP:
-        drivetrain = TunerConstants_Anemone.createDrivetrain();
-        break;
-      case ANEMONE:
-        drivetrain = TunerConstants_Anemone.createDrivetrain();
-        break;
-      case KITBOT:
-        drivetrain = TunerConstants_Anemone.createDrivetrain();
-        break;
-      case DEV:
-        drivetrain = TunerConstants_mk4n.createDrivetrain();
-        break;
-      default:
-        drivetrain = TunerConstants_Anemone.createDrivetrain();
-        break;
+    public RobotContainer() {
+        configureBindings();
     }
 
-    defualtDriveCommand = new DriveCommand(drivetrain, controller);
+    private void configureBindings() {
+        // Note that X is defined as forward according to WPILib convention,
+        // and Y is defined as to the left according to WPILib convention.
+        drivetrain.setDefaultCommand(
+            // Drivetrain will execute this command periodically
+            drivetrain.applyRequest(() ->
+                drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
+                    .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+                    .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+            )
+        );
 
-    configureBindings();
-    configureAutonomous();
-    drivetrain.setDefaultCommand(defualtDriveCommand);
-    CommandScheduler.getInstance().schedule(PathfindingCommand.warmupCommand());
+        // Idle while the robot is disabled. This ensures the configured
+        // neutral mode is applied to the drive motors while disabled.
+        final var idle = new SwerveRequest.Idle();
+        RobotModeTriggers.disabled().whileTrue(
+            drivetrain.applyRequest(() -> idle).ignoringDisable(true)
+        );
 
-    SmartDashboard.putData("Command Scheduler", CommandScheduler.getInstance());
-    addPeriodic.accept(() -> {}, 0.5);
-  }
+        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+        joystick.b().whileTrue(drivetrain.applyRequest(() ->
+            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
+        ));
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-  private void configureBindings() {}
+        // Run SysId routines when holding back/start and X/Y.
+        // Note that each routine should be run exactly once in a single log.
+        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+        joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+        joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
-  public Command getAutonomousCommand() {
-    return autoChooser.getSelected();
-  }
+        // Reset the field-centric heading on left bumper press.
+        joystick.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
 
-  private void configureAutonomous() {
-    SmartDashboard.putData("autonomous", autoChooser);
-  }
+        drivetrain.registerTelemetry(logger::telemeterize);
+    }
 
-  public void periodic() {
-    double startTime = HALUtil.getFPGATime();
-
-    startTime = HALUtil.getFPGATime();
-
-    signalList.refreshAll();
-
-    // 2
-    DogLog.log(
-        "Loop Time/Robot Container/Robot Visualizer", (HALUtil.getFPGATime() - startTime) / 1000);
-    robovisual.update();
-    startTime = HALUtil.getFPGATime();
-
-    // Log Triggers
-    DogLog.log("Current Robot", getRobot().toString());
-    DogLog.log("Match Timer", DriverStation.getMatchTime());
-  }
+    public Command getAutonomousCommand() {
+        // Simple drive forward auton
+        final var idle = new SwerveRequest.Idle();
+        return Commands.sequence(
+            // Reset our field centric heading to match the robot
+            // facing away from our alliance station wall (0 deg).
+            drivetrain.runOnce(() -> drivetrain.seedFieldCentric(Rotation2d.kZero)),
+            // Then slowly drive forward (away from us) for 5 seconds.
+            drivetrain.applyRequest(() ->
+                drive.withVelocityX(0.5)
+                    .withVelocityY(0)
+                    .withRotationalRate(0)
+            )
+            .withTimeout(5.0),
+            // Finally idle for the rest of auton
+            drivetrain.applyRequest(() -> idle)
+        );
+    }
 }
