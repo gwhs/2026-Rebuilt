b622b0c7bdbe9eb6a1553d990474c9d9e5542fc6
Emma Kadlecek <kadlecekemma@gmail.com>
2026-01-17 15:35:51 -0800

    .

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -8,8 +8,8 @@ public class FieldConstants {
   public static double BLUE_HUBY = Units.inchesToMeters(158.84);
   public static Translation2d BLUE_HUB = new Translation2d(BLUE_HUBX, BLUE_HUBY);
 
-  public static double RED_HUBX = (469.11);
-  public static double RED_HUBY = (158.84);
+  public static double RED_HUBX = Units.inchesToMeters(469.11);
+  public static double RED_HUBY = Units.inchesToMeters(158.84);
   public static Translation2d RED_HUB = new Translation2d(RED_HUBX, RED_HUBY);
 
   public static final double ALLIANCE_ZONE_LINE_BLUE = 3.977894;
@@ -17,12 +17,12 @@ public class FieldConstants {
 
   public static double BLUE_DEPOT_PASSING_X = 1.160;
   public static double BLUE_DEPOT_PASSING_Y = 6.896;
-  public static double RED_DEPOT_PASSING_X = 13.986;
-  public static double RED_DEPOT_PASSING_Y = 6.896;
+  public static double RED_OUTPOST_PASSING_X = 13.986;
+  public static double RED_OUTPOST_PASSING_Y = 6.896;
   public static double BLUE_OUTPOST_PASSING_X = 1.160;
   public static double BLUE_OUTPOST_PASSING_Y = 1.116;
-  public static double RED_OUTPOST_PASSING_X = 13.986;
-  public static double RED_OUTPOST_PASSING_Y = 1.116;
+  public static double RED_DEPOT_PASSING_X = 13.986;
+  public static double RED_DEPOT_PASSING_Y = 1.116;
 
   public static Translation2d RED_DEPOT_PASSING =
       new Translation2d(RED_DEPOT_PASSING_X, RED_DEPOT_PASSING_Y);
--- src/main/java/frc/robot/subsystems/swerve/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveSubsystem.java
@@ -22,7 +22,6 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.Subsystem;
@@ -90,9 +89,6 @@ public class SwerveSubsystem extends SwerveDrivetrain<TalonFX, TalonFX, CANcoder
     if (Utils.isSimulation()) {
       startSimThread();
     }
-    SmartDashboard.putData("align to depot", setRotationCommand(RotationTarget.PASSING_DEPOT_SIDE));
-    SmartDashboard.putData(
-        "align to outpost", setRotationCommand(RotationTarget.PASSING_OUTPOST_SIDE));
     configureAutoBuilder();
     registerTelemetry(logger::telemeterize);
   }
