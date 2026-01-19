a8dcf0fe506703c59ad7d449fea8ef78ad6958bb
Caden <cajahu8@gmail.com>
2026-01-19 10:20:42 -0800

    why is it angry

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -134,7 +134,7 @@ public class RobotContainer {
    * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
-  private void configureBindings(){
+  private void configureBindings() {
     controller.leftBumper().whileTrue(drivetrain.temporarilyDisableRotation());
   }
 
