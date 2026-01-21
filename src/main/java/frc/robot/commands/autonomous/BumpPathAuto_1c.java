d392c9e3fdb990c5e3914c3765711e199ea25282
AStormBird <birdonthemountain@gmail.com>
2026-01-21 15:57:53 -0800

    Update at 'Wed Jan 21 15:57:52 PST 2026'

--- src/main/java/frc/robot/commands/autonomous/BumpPathAuto_1c.java
+++ src/main/java/frc/robot/commands/autonomous/BumpPathAuto_1c.java
@@ -4,7 +4,6 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.EagleUtil;
