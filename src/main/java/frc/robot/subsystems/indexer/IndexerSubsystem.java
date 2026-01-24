895f4454cc24ca3de597b371bed5e883279b6c47
isaacngkh <isaacngkh@gmail.com>
2026-01-24 11:52:17 -0800

    .

--- src/main/java/frc/robot/subsystems/indexer/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerSubsystem.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.indexer2;
+package frc.robot.subsystems.indexer;
 
 import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.StatusSignalCollection;
