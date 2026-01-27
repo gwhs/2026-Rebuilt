e27d66be70264361232a45d6f42ea4cba7018d46
Niliang1 <Niliang1@s.sfusd.edu>
2026-01-26 17:38:45 -0800

    changed coordinates

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -14,10 +14,10 @@ public class FieldConstants {
 
   public static double BUMP_WIDTH = 30;
   public static double BLUE_BUMP_X1 = Units.inchesToMeters(446.91);
-  public static double BLUE_BUMP_X2 = Units.inchesToMeters(482.22 + BUMP_WIDTH);
+  public static double BLUE_BUMP_X2 = Units.inchesToMeters(446.91 + BUMP_WIDTH);
 
   public static double RED_BUMP_X1 = Units.inchesToMeters(121.30);
-  public static double RED_BUMP_X2 = Units.inchesToMeters(156.61 + BUMP_WIDTH);
+  public static double RED_BUMP_X2 = Units.inchesToMeters(121.30 + BUMP_WIDTH);
 
   public static final double ALLIANCE_ZONE_LINE_BLUE = 3.977894;
   public static final double ALLIANCE_ZONE_LINE_RED = 12.535154;
