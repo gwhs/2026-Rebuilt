abcb05c932dc681d01dbe38399d74117062c7dc4
Bella Tan <belltan635@gmail.com>
2026-01-28 19:54:41 -0800

    Ground roller subsystem (#59)

---
+++ src/main/java/frc/robot/subsystems/groundIntakeRoller/GroundIntakeRollerSubsystem.java
@@ -0,0 +1,53 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.groundIntakeRoller;
+
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.StatusSignalCollection;
+import dev.doglog.DogLog;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class GroundIntakeRollerSubsystem extends SubsystemBase {
+
+  private GroundIntakeRollerIO groundIntakeRollerIO;
+  private double groundRollerVoltage;
+
+  public GroundIntakeRollerSubsystem(
+      CANBus rioCanbus, CANBus canivoreCanbus, StatusSignalCollection signal) {
+    if (RobotBase.isSimulation()) {
+      groundIntakeRollerIO = new GroundIntakeRollerIOSim();
+    } else {
+      groundIntakeRollerIO = new GroundIntakeRollerIOReal(rioCanbus, canivoreCanbus, signal);
+    }
+  }
+
+  public Command runVoltage(double voltage) {
+    return this.runOnce(
+        () -> {
+          groundIntakeRollerIO.runVoltage(voltage);
+          groundRollerVoltage = voltage;
+        });
+  }
+
+  public Command stopIntake() {
+    return runVoltage(0);
+  }
+
+  public Command startIntake() {
+    return runVoltage(GroundIntakeRollerConstants.DEFAULT_INTAKE_VOLTAGE);
+  }
+
+  public Command reverseIntake() {
+    return runVoltage(GroundIntakeRollerConstants.REVERSE_INTAKE_VOLTAGE);
+  }
+
+  @Override
+  public void periodic() {
+    groundIntakeRollerIO.periodic();
+    DogLog.log("Ground Intake Roller/Goal Voltage", groundRollerVoltage);
+  }
+}
