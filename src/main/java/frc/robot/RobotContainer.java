3cb98435dbda08ad32881c426848c3d9e89f7a6a
Caden <cajahu8@gmail.com>
2026-01-31 10:33:48 -0800

    qol

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -260,7 +260,7 @@ public class RobotContainer {
     return Commands.parallel(
         drivetrain.setRotationCommand(RotationTarget.HUB),
         shooter.cruiseControl(),
-        indexer.index().onlyWhile(shooter.isAtGoalVelocity_Hub.and(drivetrain.isFacingGoal)));
+        indexer.index().onlyWhile(shooter.isAtGoalVelocity_Hub.and(drivetrain.isFacingGoal).or(controller.leftTrigger())).repeatedly());
   }
 
   public Command shootDepot() {
@@ -269,7 +269,7 @@ public class RobotContainer {
         shooter.cruiseControl(),
         indexer
             .index()
-            .onlyWhile(shooter.isAtGoalVelocity_Passing.and(drivetrain.isFacingGoalPassing)));
+            .onlyWhile(shooter.isAtGoalVelocity_Passing.and(drivetrain.isFacingGoalPassing).or(controller.leftTrigger())).repeatedly());
   }
 
   public Command shootOutpost() {
@@ -278,7 +278,7 @@ public class RobotContainer {
         shooter.cruiseControl(),
         indexer
             .index()
-            .onlyWhile(shooter.isAtGoalVelocity_Passing.and(drivetrain.isFacingGoalPassing)));
+            .onlyWhile(shooter.isAtGoalVelocity_Passing.and(drivetrain.isFacingGoalPassing).or(controller.leftTrigger())).repeatedly());
   }
 
   // TODO: add ground intake when said subsystem is added
