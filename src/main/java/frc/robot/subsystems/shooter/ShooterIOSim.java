2646316b9e5acc6a680ba7794d32458940b1a36f
Emma <kadlecekemma@gmail.com>
2026-02-07 12:56:08 -0800

    .

--- src/main/java/frc/robot/subsystems/shooter/ShooterIOSim.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOSim.java
@@ -1,18 +1,41 @@
 package frc.robot.subsystems.shooter;
 
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+
 public class ShooterIOSim implements ShooterIO {
 
-  @Override
-  public void runVelocity(double rotationsPerSecond) {}
+  private FlywheelSim motor =
+      new FlywheelSim(
+          LinearSystemId.createFlywheelSystem(DCMotor.getKrakenX60Foc(1), 0.001, 1),
+          DCMotor.getKrakenX60Foc(2));
+
+  private double targetVelocity = 0; // rotationsPerSecond
 
   @Override
-  public void runVoltage(double voltage) {}
-
-  @Override
-  public double getVelocity() {
-    return 0.0;
+  public void runVelocity(double rotationsPerSecond) {
+    targetVelocity = rotationsPerSecond;
   }
 
   @Override
-  public void periodic() {}
+  public void runVoltage(double voltage) {
+    motor.setInputVoltage(voltage);
+  }
+
+  @Override
+  public double getVelocity() {
+    return motor.getAngularVelocity().in(RotationsPerSecond);
+  }
+
+  @Override
+  public void periodic() {
+    motor.update(.020);
+    double error = targetVelocity - getVelocity();
+    double kP = 50;
+    double voltage = kP * error;
+    motor.setInputVoltage(voltage);
+  }
 }
