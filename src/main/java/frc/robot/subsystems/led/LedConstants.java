6163dac215892444d57adca7094f9d3a5d6a8b0a
EthanZ <roblox.easy123456@gmail.com>
2026-02-09 17:47:09 -0800

    Update at 'Mon Feb 09 17:47:08 PST 2026'

--- src/main/java/frc/robot/subsystems/led/LedConstants.java
+++ src/main/java/frc/robot/subsystems/led/LedConstants.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems.led;
 import com.ctre.phoenix6.CANBus;
 
 class LedConstants {
-  public static int CANdleID = 23;
+  public static int CANdleID = 50;
   public static CANBus CANdleCANBus = new CANBus("rio");
   public static int IndexMax = 100; // TODO: change to actual size
 }
--- src/main/java/frc/robot/subsystems/led/LedSubsystem.java
+++ src/main/java/frc/robot/subsystems/led/LedSubsystem.java
@@ -1,15 +1,12 @@
 package frc.robot.subsystems.led;
 
 import com.ctre.phoenix6.configs.CANdleConfiguration;
-import com.ctre.phoenix6.controls.RgbFadeAnimation;
 import com.ctre.phoenix6.controls.SolidColor;
 import com.ctre.phoenix6.hardware.CANdle;
 import com.ctre.phoenix6.signals.RGBWColor;
 import com.ctre.phoenix6.signals.StatusLedWhenActiveValue;
 import com.ctre.phoenix6.signals.StripTypeValue;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -22,27 +19,29 @@ public class LedSubsystem extends SubsystemBase {
     conf.LED.BrightnessScalar = 1.0;
     conf.CANdleFeatures.StatusLedWhenActive = StatusLedWhenActiveValue.Disabled;
 
-    for (int i = 0; i < 6; i++)
-    {
+    for (int i = 0; i < 6; i++) {
       candle.getConfigurator().apply(conf);
     }
-    
+
     SmartDashboard.putData("solid green", solidGreen());
   }
 
-  private final SolidColor[] white = new SolidColor[] {
+  private final SolidColor[] white =
+      new SolidColor[] {
         new SolidColor(0, LedConstants.IndexMax).withColor(new RGBWColor(255, 255, 255))
-    };
+      };
 
-    private final SolidColor[] green = new SolidColor[] {
+  private final SolidColor[] green =
+      new SolidColor[] {
         new SolidColor(0, LedConstants.IndexMax).withColor(new RGBWColor(0, 255, 0))
-    };
+      };
 
   public Command solidGreen() {
-        return run(() -> {
-            for (var solidColor : green) {
-                candle.setControl(solidColor);
-            }
+    return run(
+        () -> {
+          for (var solidColor : green) {
+            candle.setControl(solidColor);
+          }
         });
   }
 }
