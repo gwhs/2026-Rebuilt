75ae41c6f5b304b7a48e62ae0ffdc9fba083bc41
white <2243119644@qq.com>
2026-02-23 19:16:47 -0800

    added moving ground intake in sim

--- src/main/java/frc/robot/RobotVisualizer.java
+++ src/main/java/frc/robot/RobotVisualizer.java
@@ -4,6 +4,7 @@ import dev.doglog.DogLog;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.groundIntakeLinearExtension.GroundIntakeLinearExtensionConstants;
 import frc.robot.subsystems.groundIntakeLinearExtension.GroundIntakeLinearExtensionSubsystem;
 
 public class RobotVisualizer extends SubsystemBase {
@@ -15,7 +16,7 @@ public class RobotVisualizer extends SubsystemBase {
 
   @Override
   public void periodic() {
-    double groundIntakeExtension = linearExt.getRotation() * 0.035; // 35cm full out
+    double groundIntakeExtension = linearExt.getRotation() * 0.35 / GroundIntakeLinearExtensionConstants.EXTENSION_ROTATION; // 35cm full out
 
     Pose3d groundIntakeRack = new Pose3d(groundIntakeExtension, 0, 0, new Rotation3d());
     Pose3d groundIntakePosition = new Pose3d(groundIntakeExtension, 0, 0, new Rotation3d());
