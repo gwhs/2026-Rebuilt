065cb83018280faafc9c361f222ceceaf752ac4d
foodieey <EmilyYYu@hotmail.com>
2026-02-25 15:19:01 -0800

    Update at 'Wed Feb 25 15:19:01 PST 2026'

--- src/main/java/frc/robot/subsystems/groundIntakeLinearExtension/GroundIntakeLinearExtensionIOReal.java
+++ src/main/java/frc/robot/subsystems/groundIntakeLinearExtension/GroundIntakeLinearExtensionIOReal.java
@@ -136,6 +136,8 @@ public class GroundIntakeLinearExtensionIOReal implements GroundIntakeLinearExte
               AlertType.kError)
           .set(true);
     }
+
+    setPosition(0);
   }
 
   @Override
