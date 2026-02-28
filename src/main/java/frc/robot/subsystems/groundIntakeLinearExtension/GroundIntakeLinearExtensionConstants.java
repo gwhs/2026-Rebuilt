670d55a4fc0c78c1ab4addfaa9c1ed3872155d4b
foodieey <EmilyYYu@hotmail.com>
2026-02-28 10:11:09 -0800

    changes

--- src/main/java/frc/robot/subsystems/groundIntakeLinearExtension/GroundIntakeLinearExtensionConstants.java
+++ src/main/java/frc/robot/subsystems/groundIntakeLinearExtension/GroundIntakeLinearExtensionConstants.java
@@ -3,8 +3,8 @@ package frc.robot.subsystems.groundIntakeLinearExtension;
 public class GroundIntakeLinearExtensionConstants {
   public static final int MOTOR_ID = 32;
 
-  public static final double MAX_VELOCITY = 1.2; // THINGSTODO
-  public static final double MAX_ACCELERATION = 3.0; // TODO
+  public static final double MAX_VELOCITY = 6; // THINGSTODO
+  public static final double MAX_ACCELERATION = 4.0; // TODO
 
   public static final double MAX_ROTATION = 10; // TODO
   public static final double MIN_ROTATION = 0;
