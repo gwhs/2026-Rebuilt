8d44531a06ff9d10ee1299de4841d7de12b825ba
foodieey <EmilyYYu@hotmail.com>
2026-03-10 18:01:49 -0700

    Update at 'Tue Mar 10 18:01:48 PDT 2026'

--- src/main/java/frc/robot/subsystems/groundIntakeLinearExtension/GroundIntakePivotIOReal.java
+++ src/main/java/frc/robot/subsystems/groundIntakeLinearExtension/GroundIntakePivotIOReal.java
@@ -124,9 +124,8 @@ public class GroundIntakePivotIOReal implements GroundIntakeLinearExtensionIO {
 
     CANcoderConfiguration encoderConfig = new CANcoderConfiguration();
     encoderConfig.MagnetSensor.AbsoluteSensorDiscontinuityPoint = 0.5;
-    encoderConfig.MagnetSensor.MagnetOffset = -0.124512;
-    encoderConfig.MagnetSensor.SensorDirection =
-        SensorDirectionValue.CounterClockwise_Positive;
+    encoderConfig.MagnetSensor.MagnetOffset = -0.124512 - 0.0627;
+    encoderConfig.MagnetSensor.SensorDirection = SensorDirectionValue.CounterClockwise_Positive;
 
     for (int i = 0; i < 5; i++) {
       status = groundIntakePivotEncoder.getConfigurator().apply(encoderConfig);
