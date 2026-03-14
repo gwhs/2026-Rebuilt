dbcfaca821c015a22430a4bc47a0109d413f690f
EthanZ <roblox.easy123456@gmail.com>
2026-03-14 11:58:22 -0700

    Update at 'Sat Mar 14 11:58:22 PDT 2026'

--- src/main/java/frc/robot/commands/autonomous/NeutralAutos.java
+++ src/main/java/frc/robot/commands/autonomous/NeutralAutos.java
@@ -78,6 +78,9 @@ public class NeutralAutos extends SequentialCommandGroup {
 
       addCommands(
           Commands.sequence(
+                  Commands.waitSeconds(5).deadlineFor(shooter.runVelocity(65), indexer.index()),
+                  shooter.runVoltage(0),
+                  indexer.runVoltage(0),
                   AutoBuilder.resetOdom(startingPose).onlyIf(() -> RobotBase.isSimulation()),
                   cyclePath(cycle, cycletwo, true),
                   cyclePath(cycletwo, cycletwo, false).onlyIf(() -> twoCycle),
