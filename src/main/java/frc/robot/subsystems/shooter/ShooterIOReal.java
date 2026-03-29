6bc40e67dfa9ee6451d440a4a7a227d65af9fe91
foodieey <EmilyYYu@hotmail.com>
2026-03-28 18:30:33 -0700

    Update at 'Sat Mar 28 18:30:32 PDT 2026'

--- src/main/java/frc/robot/subsystems/shooter/ShooterIOReal.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOReal.java
@@ -198,7 +198,7 @@ public class ShooterIOReal implements ShooterIO {
 
     TalonFXConfiguration talonFXConfig = new TalonFXConfiguration();
 
-    talonFXConfig.CurrentLimits.StatorCurrentLimit = 55;
+    talonFXConfig.CurrentLimits.StatorCurrentLimit = 50;
     talonFXConfig.CurrentLimits.StatorCurrentLimitEnable = true;
 
     talonFXConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
