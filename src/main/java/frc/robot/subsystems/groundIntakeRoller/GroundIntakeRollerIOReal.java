81469b33a94f13001c75a505f8ad7d7e70c9ca86
foodieey <EmilyYYu@hotmail.com>
2026-03-28 19:50:33 -0700

    Update at 'Sat Mar 28 19:50:33 PDT 2026'

--- src/main/java/frc/robot/subsystems/groundIntakeRoller/GroundIntakeRollerIOReal.java
+++ src/main/java/frc/robot/subsystems/groundIntakeRoller/GroundIntakeRollerIOReal.java
@@ -38,7 +38,7 @@ public class GroundIntakeRollerIOReal implements GroundIntakeRollerIO {
   private final StatusSignal<Temperature> motor2Temp;
 
   private final Follower controlRequest =
-      new Follower(GroundIntakeRollerConstants.MOTOR_1_ID, MotorAlignmentValue.Opposed);
+      new Follower(GroundIntakeRollerConstants.MOTOR_2_ID, MotorAlignmentValue.Opposed);
 
   private final Alert motor1NotConnectedAlert =
       new Alert("Ground Intake Roller Motor 1 Not Connected ", AlertType.kError);
@@ -116,7 +116,7 @@ public class GroundIntakeRollerIOReal implements GroundIntakeRollerIO {
   }
 
   public void runVoltage(double voltage) {
-    motor1.setVoltage(0);
+    motor1.setControl(controlRequest);
     motor2.setVoltage(voltage);
   }
 
