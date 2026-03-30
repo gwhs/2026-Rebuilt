5de5fe20aafd40be713b509be05aad76b7cc0c19
Caden <Cachen15@s.sfusd.edu>
2026-03-29 17:20:46 -0700

    bye

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -389,7 +389,7 @@ public class RobotContainer {
     controller.leftBumper().onTrue(drivetrain.setRotationCommand(RotationTarget.NORMAL));
     // drivetrain.isOnBump.whileTrue(drivetrain.temporarilyDisableRotation());
 
-    controller.rightTrigger().and(drivetrain.isInAllianceZone).debounce(0.1).whileTrue(shootHub());
+    controller.rightTrigger().and(drivetrain.isInAllianceZone).whileTrue(shootHub());
 
     controller.rightTrigger().whileTrue(agitateGroundIntake());
 
@@ -609,7 +609,7 @@ public class RobotContainer {
             drivetrain.setRotationCommand(RotationTarget.HUB),
             drivetrain.setSlowMode(0.5, 1),
             Commands.waitUntil(
-                drivetrain.isFacingGoal.and(isHubActive).or(controller.leftTrigger())),
+                drivetrain.isFacingGoal.debounce(0.1).and(isHubActive).or(controller.leftTrigger())),
             Commands.parallel(
                     indexer.index(), shooter.cruiseControl(), EagleUtil.shootInSim(drivetrain))
                 .onlyWhile(drivetrain.isFacingGoal.and(isHubActive).or(controller.leftTrigger()))
