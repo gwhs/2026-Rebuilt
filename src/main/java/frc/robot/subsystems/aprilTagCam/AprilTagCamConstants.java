c254b0b5d890fd48119c68b423b92c168aab227c
foodieey <EmilyYYu@hotmail.com>
2026-04-02 12:20:52 -0700

    hi

--- src/main/java/frc/robot/subsystems/aprilTagCam/AprilTagCamConstants.java
+++ src/main/java/frc/robot/subsystems/aprilTagCam/AprilTagCamConstants.java
@@ -56,7 +56,7 @@ public class AprilTagCamConstants {
   public static final double MAX_X_VALUE = 690.87;
   public static final double MAX_Y_VALUE = 317.00;
   public static final double SINGLE_APRILTAG_MAX_DISTANCE = 3.0;
-  public static final double MULTI_APRILTAG_MAX_DISTANCE = 4.5;
+  public static final double MULTI_APRILTAG_MAX_DISTANCE = 7.0;
   public static final double MAX_VELOCITY = 4;
   public static final double MAX_ROTATION = Math.PI;
 }
