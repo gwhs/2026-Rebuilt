6a7e807d29b891c5c4f6e4332ee386d63ea72200
EthanZ <roblox.easy123456@gmail.com>
2026-04-02 19:32:00 -0700

    hi

--- src/main/java/frc/robot/ShotCalculator.java
+++ src/main/java/frc/robot/ShotCalculator.java
@@ -16,29 +16,29 @@ public class ShotCalculator {
   private static final InterpolatingDoubleTreeMap timeOfFlight = new InterpolatingDoubleTreeMap();
 
   static {
-    backBeltMap.put(1.07, 41.0);
-    backBeltMap.put(1.322, 42.0);
-    backBeltMap.put(1.498, 42.0);
-    backBeltMap.put(1.600, 44.0);
-    backBeltMap.put(1.775, 46.0);
-    backBeltMap.put(2.075, 48.0);
-    backBeltMap.put(2.425, 51.0);
-    backBeltMap.put(2.835, 58.0);
-    backBeltMap.put(3.0, 58.0);
-    backBeltMap.put(3.5, 64.0);
-    backBeltMap.put(4.0, 68.0);
+    backBeltMap.put(1.07, 40.0);
+    backBeltMap.put(1.322, 41.0);
+    backBeltMap.put(1.498, 41.0);
+    backBeltMap.put(1.600, 43.0);
+    backBeltMap.put(1.775, 45.0);
+    backBeltMap.put(2.075, 47.0);
+    backBeltMap.put(2.425, 50.0);
+    backBeltMap.put(2.835, 57.0);
+    backBeltMap.put(3.0, 57.0);
+    backBeltMap.put(3.5, 63.0);
+    backBeltMap.put(4.0, 67.0);
 
-    frontBeltMap.put(1.07, 44.0);
-    frontBeltMap.put(1.32, 45.0);
-    frontBeltMap.put(1.498, 46.0);
-    frontBeltMap.put(1.600, 48.0);
-    frontBeltMap.put(1.775, 51.0);
-    frontBeltMap.put(2.075, 54.0);
-    frontBeltMap.put(2.425, 58.0);
-    frontBeltMap.put(2.835, 63.0);
-    frontBeltMap.put(3.0, 63.0);
-    frontBeltMap.put(3.5, 66.0);
-    frontBeltMap.put(4.0, 72.0);
+    frontBeltMap.put(1.07, 43.0);
+    frontBeltMap.put(1.32, 44.0);
+    frontBeltMap.put(1.498, 45.0);
+    frontBeltMap.put(1.600, 47.0);
+    frontBeltMap.put(1.775, 50.0);
+    frontBeltMap.put(2.075, 53.0);
+    frontBeltMap.put(2.425, 57.0);
+    frontBeltMap.put(2.835, 62.0);
+    frontBeltMap.put(3.0, 62.0);
+    frontBeltMap.put(3.5, 65.0);
+    frontBeltMap.put(4.0, 71.0);
 
     timeOfFlight.put(1.97, 1.26);
     timeOfFlight.put(2.560, 1.31);
