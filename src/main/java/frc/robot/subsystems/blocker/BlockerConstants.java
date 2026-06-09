7b449e2cc9bc3c7a36fa06c20d9defa693a74f18
etzhang4 <roblox.easy123456@gmail.com>
2026-06-09 16:07:16 -0700

    blocker (#166)

---
+++ src/main/java/frc/robot/subsystems/blocker/BlockerConstants.java
@@ -0,0 +1,13 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.blocker;
+
+public class BlockerConstants {
+
+  public static final int MOTOR_1_ID = 61;
+
+  public static final double MAX_ROTATION = 2.8;
+  public static final double MIN_ROTATION = 0;
+}
