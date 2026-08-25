7e6214ddae0866cc8c46399f14940a8ffdae283a
Owen Kuo <owennkuo@gmail.com>
2026-08-24 17:47:38 -0700

    Vision Code Cleanup (#173)
    * clean up old code
    
    * remove nonexistent import
    
    * remove old methods
    
    ---------
    
    Co-authored-by: Isaac Ng <isaacngkh@gmail.com>

--- src/main/java/frc/robot/subsystems/swerve/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveSubsystem.java
@@ -17,14 +17,11 @@ import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 import dev.doglog.DogLog;
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.numbers.N1;
-import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -40,7 +37,6 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.EagleUtil;
 import frc.robot.FieldConstants;
 import frc.robot.commands.AlignToPose;
-import frc.robot.subsystems.aprilTagCam.AprilTagHelp;
 import java.util.function.Supplier;
 
 /**
@@ -544,15 +540,6 @@ public class SwerveSubsystem extends SwerveDrivetrain<TalonFX, TalonFX, CANcoder
               this.disableAutoRotate = false;
             })
         .withName("Disable AutoRotation");
-  }
-
-  public void addVisionMeasurent(AprilTagHelp helper) {
-
-    Pose2d pos = helper.pos;
-    Matrix<N3, N1> sd = helper.sd;
-    double timestamp = helper.timestamp;
-
-    super.addVisionMeasurement(pos, timestamp, sd);
   }
 
   public Pose2d getCachedVirtualTarget() {
